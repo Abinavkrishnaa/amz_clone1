c33722fb998475bfeafdc6e54108fd92aad883d9
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2023-08-20 23:43:51 +0530

    second commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,61 +1,127 @@
-import logo from "@/images/logo.png"
-import Image from "next/image"
-import cartIcon from "@/images/cartIcon.png"
-import {SlLocationPin} from "react-icons/sl";
-import {HiOutlineSearch} from "react-icons/hi";
-import {BiCaretDown} from "react-icons/bi";
+import Image from "next/image";
+import logo from "../../images/logo.png";
+import cartIcon from "../../images/cartIcon.png";
+import { BiCaretDown } from "react-icons/bi";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlLocationPin } from "react-icons/sl";
 import Link from "next/link";
-import { useSelector , useDispatch } from "react-redux";
-import { StateProps } from "../../../type";
+import { useSelector, useDispatch } from "react-redux";
+import { StateProps, StoreProduct } from "../../../type";
 import { useSession, signIn, signOut } from "next-auth/react";
-import {useEffect,useState} from "react"
+import { useEffect, useState } from "react";
 import { addUser } from "@/store/nextSlice";
-
-const Header=()=> {
-  const { data: session} = useSession();
+import SearchProducts from "../SearchProducts";
+const Header = () => {
+  const { data: session } = useSession();
   const [allData, setAllData] = useState([]);
-  const {productData,favoriteData,userInfo,allProducts} = useSelector((state: StateProps )=>state.next);
+  const { productData, favoriteData, userInfo, allProducts } = useSelector(
+    (state: StateProps) => state.next
+  );
   const dispatch = useDispatch();
-  useEffect(()=>{
+  useEffect(() => {
     setAllData(allProducts.allProducts);
   }, [allProducts]);
-  
-  useEffect(()=>{
-    if(session){
-      dispatch(addUser({
-        name:session?.user?.name,
-        email:session?.user?.email,
-        image:session?.user?.image
-      }))
-
+  useEffect(() => {
+    if (session) {
+      dispatch(
+        addUser({
+          name: session?.user?.name,
+          email: session?.user?.email,
+          image: session?.user?.image,
+        })
+      );
     }
-  },[session])
-  return (
+  }, [session]);
 
+  // Search area
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    const filtered = allData.filter((item: StoreProduct) =>
+      item.title.toLocaleLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  }, [searchQuery]);
+
+  return (
     <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
-        <div className="w-full h-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
-          <Link href={"/"} className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 
-          flex mt-1 items-center justify-center h-[70%]">
-            { <Image src={logo} alt="logoImg" className="w-28 object-cover mt-1" /> }
-          
-          </Link>
-          <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 
-          mt-1 items-center justify-center h-[70%] hidden xl:inline-flex gap-1">
-            <SlLocationPin />
-            <div className="text-xs">
-              <p>Deliver To</p>
-              <p className="text-white font-bold uppercase">Your Location</p>
+      <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
+        {/* logo */}
+        <Link
+          href={"/"}
+          className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]"
+        >
+          <Image className="w-28 object-cover mt-1" src={logo} alt="logoImg" />
+        </Link>
+        {/* delivery */}
+        <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 items-center justify-center h-[70%] hidden xl:inline-flex gap-1">
+          <SlLocationPin />
+          <div className="text-xs">
+            <p>Deliver to</p>
+            <p className="text-white font-bold uppercase">USA</p>
+          </div>
+        </div>
+        {/* serchbar */}
+        <div className="flex-1 h-10 hidden md:inline-flex items-center justify-between relative">
+          <input
+            onChange={handleSearch}
+            value={searchQuery}
+            className="w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none focus-visible:border-amazon_yellow"
+            type="text"
+            placeholder="Search  products"
+          />
+          <span className="w-12 h-full bg-amazon_yellow text-black text-2xl flex items-center justify-center absolute right-0 rounded-tr-md rounded-br-md">
+            <HiOutlineSearch />
+          </span>
+          {/* ========== Searchfield ========== */}
+          {searchQuery && (
+            <div className="absolute left-0 top-12 w-full mx-auto max-h-96 bg-gray-200 rounded-lg overflow-y-scroll cursor-pointer text-black">
+              {filteredProducts.length > 0 ? (
+                <>
+                  {searchQuery &&
+                    filteredProducts.map((item: StoreProduct) => (
+                      <Link
+                        key={item._id}
+                        className="w-full border-b-[1px] border-b-gray-400 flex items-center gap-4"
+                        href={{
+                          pathname: `${item._id}`,
+                          query: {
+                            _id: item._id,
+                            brand: item.brand,
+                            category: item.category,
+                            description: item.description,
+                            image: item.image,
+                            isNew: item.isNew,
+                            oldPrice: item.oldPrice,
+                            price: item.price,
+                            title: item.title,
+                          },
+                        }}
+                        onClick={() => setSearchQuery("")}
+                      >
+                        <SearchProducts item={item} />
+                      </Link>
+                    ))}
+                </>
+              ) : (
+                <div className="bg-gray-50 flex items-center justify-center py-10 rounded-lg shadow-lg">
+                  <p className="text-xl font-semibold animate-bounce">
+                    Nothing is matches with your search keywords. Please try
+                    again!
+                  </p>
+                </div>
+              )}
             </div>
-          </div>
-          <div className="flex-1 h-10 hidden md:inline-flex items-center justify-between relative">
-            <input className="w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none
-            focus-visible:border-amazon_yellow " type="text" placeholder="Search Products" />
-            <span className="w-12 h-full bg-amazon_yellow text-black text-2xl flex items-center justify-center absolute right-0 rounded-tr-md
-            rounded-br-md">
-              <HiOutlineSearch />
-            </span>
-          </div>
-          {userInfo ? (
+          )}
+          {/* ========== Searchfield ========== */}
+        </div>
+        {/* signin */}
+        {userInfo ? (
           <div className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] gap-1">
             <img
               src={userInfo.image}
@@ -81,9 +147,10 @@ const Header=()=> {
             </p>
           </div>
         )}
-          <Link
+        {/* fovorite */}
+        <Link
           href={"/favorite"}
-          className="text-xs text-gray-100 flex fl  ex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative"
+          className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative"
         >
           <p>Marked</p>
           <p className="text-white font-bold">& Favorite</p>
@@ -93,18 +160,24 @@ const Header=()=> {
             </span>
           )}
         </Link>
-          <Link href={"/cart"} className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative">
-            <Image className="w-auto object-cover h-8" src={cartIcon} alt="cartImg" />
-            <p className="text-xs text-white font-bold mt-3">Cart</p>
-            <span className="absolute text-amazon_yellow text-xs top-2 left-[39px] font-semibold"> 
-              {productData ? productData.length : 0} </span>
-          </Link>
-
-        
-        
-        </div>
+        {/* cart */}
+        <Link
+          href={"/cart"}
+          className="flex items-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative"
+        >
+          <Image
+            className="w-auto object-cover h-8"
+            src={cartIcon}
+            alt="cartImg"
+          />
+          <p className="text-xs text-white font-bold mt-3">Cart</p>
+          <span className="absolute text-amazon_yellow text-sm top-2 left-[29px] font-semibold">
+            {productData ? productData.length : 0}
+          </span>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

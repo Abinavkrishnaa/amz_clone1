fcc41f214f01cc8615430968aa49c9559d24adca
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2023-08-24 18:23:12 +0530

    commitnew

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { StoreProduct } from "../../../type";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// check
 
 export default async function handler(
   req: NextApiRequest,
